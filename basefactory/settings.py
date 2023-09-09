5b82b609935291d83dd899e90f9ba0a8d5226ba5
Derek Smith <DerekSmith@HarmonyFactory.com>
2023-09-09 13:53:54 +1000

    Added debug toolbar 'local_infile': True for mysql db router instsalled (default=mysql, sqlite=sqlite)

--- basefactory/settings.py
+++ basefactory/settings.py
@@ -12,16 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
 HOSTNAME = os.environ.get('HOSTNAME')
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -34,7 +32,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'basefactory.urls'
@@ -79,28 +78,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'basefactory.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
 
-'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'harmonyfactory',
         'USER': 'harmonyfactory',
         'PASSWORD': 'semlor.choux.dasauto',
         'HOST': 'localhost',  # Set the MySQL host, e.g., 'localhost' or '127.0.0.1'
         'PORT': '',  # Leave empty to use the default MySQL port (3306)
+        'OPTIONS': {
+            'local_infile': True,
+        },
     },
-
 
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,7 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -132,7 +130,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -143,8 +140,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 AUTH_USER_MODEL = 'accounts.User'
 
 DATABASE_ROUTERS = ['basefactory.routers.DatabaseRouter']
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
